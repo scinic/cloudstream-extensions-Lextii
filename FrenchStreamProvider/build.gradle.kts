41f70ddce99ef85c7de870cde775c0f85a669e61
Lextii <111401519+Lextii@users.noreply.github.com>
2023-01-25 23:05:39 +0100

    Update build.gradle.kts

--- FrenchStreamProvider/build.gradle.kts
+++ FrenchStreamProvider/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 1
+version = 2
 
 
 cloudstream {
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
      description = "FRENCH STREAM en plus d'être un site efficace et plaisant dispose d'un contenu visuel diversifié"
-     authors = listOf("Sarlay", "Eddy976, Lextii")
+     authors = listOf("Lextii")
 
     /**
      * Status int as the following:
